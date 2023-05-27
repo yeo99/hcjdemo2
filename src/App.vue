a39f476511fe91fb346406276757af21c9f2349b
yeo99 <qw6375@gmail.com>
2023-05-28 03:08:50 +0900

    [TEST] node-forge CDN 테스트

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,30 @@
 <template>
   <div>
-    <span>TEST</span>
+    <span>{{ md }}</span>
+    
   </div>
 </template>
-<script>
 
+<script>
+  export default {
+    name: 'App',
+
+    data() {
+      return {
+        
+      }
+      
+    },
+
+    mounted() {
+      md = forge.md.md5.create()
+      md.update('The quick brown fox jumps over the lazy dog')
+    },
+
+    methods: {
+
+    },
+  }
 </script>
 <style scoped>
   @import url(./assets/css/normalize.css);
